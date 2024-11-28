45ba5f0f844459ce8950bfaf7eca986218e1e280
Siyi Zhang <yueyinglan512@gmail.com>
2024-11-28 22:50:55 +0800

    Update stream.py

--- home/siyi/stream.py
+++ home/siyi/stream.py
@@ -93,7 +93,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     self.client_address, str(e))
         elif self.path.startswith('figures/'):
             try:
-                file_path = '/var/www/html' + self.path
+                file_path = '/var/www/html/' + self.path
                 with open(file_path, 'rb') as f:
                     self.send_response(200)
                     if self.path.endswith('.png'):
